6db92c327965a0828d4a0f2bee69f4d1a98309e6
Simon Frändén <simon.franden@hotmail.com>
2023-04-18 13:54:40 +0200

    Add project files.

---
+++ ASPLabb1/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using ASPLabb1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPLabb1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder
+            .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ASPLabb1DB;TrustServerCertificate=True;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
---
+++ ASPLabb1/Migrations/20230418115229_initialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPLabb1.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
